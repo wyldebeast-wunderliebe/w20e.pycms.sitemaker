f78cd1cc3135c2bfcf652153cfd86b3c43891764
Duco Dokter <dokter@w20e.com>
2012-07-05 10:45:47 +0200

    Simpler init for pycms app

--- w20e/pycms/sitemaker/skel/+package+/__init__.py
+++ w20e/pycms/sitemaker/skel/+package+/__init__.py
@@ -1,25 +1,6 @@
-from pyramid.config import Configurator
-import pyramid_zcml
-from w20e.pycms import appmaker, root_factory
+from w20e.pycms import make_pycms_app
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
 
-    config = Configurator(root_factory=root_factory, settings=settings)
-    zcml_file = settings.get('configure_zcml', 'configure.zcml')
-
-    config.hook_zca()
-    config.include(pyramid_zcml)
-    config.include('pyramid_mailer')
-
-    config.load_zcml('w20e.pycms:bootstrap.zcml')
-    config.commit()
-    
-    config.load_zcml(zcml_file)
-    config.commit()
-
-    appmaker(config)
-
-    return config.make_wsgi_app()
+    return make_pycms_app(__package__, **settings)
